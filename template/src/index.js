12bef21455b7064cddc8ae54ffeacc2a51eb51cf
santake <takeshi.sannomiya@gmail.com>
2022-04-04 13:29:01 +0900

    changed index.js to be able to use on React ver.18

--- template/src/index.js
+++ template/src/index.js
@@ -1,3 +1,12 @@
+/* since React ver 18 */
+import App from './App';
+import {createRoot} from "react-dom/client";
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App/>);
+
+/* till React ver 17:
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
@@ -5,3 +14,4 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+*/
